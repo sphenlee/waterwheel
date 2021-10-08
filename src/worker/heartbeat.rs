590d0a2556be654bbe0e50690931e4ae1bae2154
Steve Lee <slee@atlassian.com>
2021-10-09 01:09:24 +1100

    heartbeat: refactor response checking logic

--- src/worker/heartbeat.rs
+++ src/worker/heartbeat.rs
@@ -3,12 +3,12 @@ use crate::messages::WorkerHeartbeat;
 use anyhow::Result;
 
 use chrono::Utc;
-use tracing::{debug, trace, warn, error, info};
+use tracing::{debug, trace, warn, error};
 
 use super::{RUNNING_TASKS, TOTAL_TASKS, WORKER_ID};
-use reqwest::{StatusCode, Url, Response};
+use reqwest::{StatusCode, Url};
 
-pub async fn post_heartbeat(client: &reqwest::Client) -> Result<Response> {
+pub async fn post_heartbeat(client: &reqwest::Client) -> Result<bool> {
     let server_addr = config::get().server_addr.as_ref();
     let url = Url::parse(server_addr)?.join("int-api/heartbeat")?;
 
@@ -23,30 +23,40 @@ pub async fn post_heartbeat(client: &reqwest::Client) -> Result<Response> {
             version: GIT_VERSION.to_owned(),
         })
         .send()
-        .await?;
+        .await;
 
-    Ok(resp)
+    match resp {
+        Ok(resp) if resp.status() == StatusCode::OK => {
+            trace!("heartbeat: OK");
+            Ok(true)
+        }
+        Ok(resp) => {
+            let status = resp.status();
+            let body = resp.text().await?;
+            warn!("heartbeat: {}", status);
+            debug!("heartbeat: {}", body);
+            Ok(false)
+        }
+        Err(err) => {
+            if err.is_connect() {
+                warn!("heartbeat failed: could not connect to the server");
+            } else if err.is_timeout() {
+                warn!("heartbeat failed: server timed out");
+            } else {
+                warn!("heartbeat failed: unexpected reason {}", err);
+            }
+            debug!("heartbeat failed: {}", err);
+            Ok(false)
+        }
+    }
 }
 
 pub async fn heartbeat() -> Result<!> {
     let client = reqwest::Client::new();
 
     loop {
-        trace!("posting heartbeat");
-        match post_heartbeat(&client).await {
-            Ok(resp) if resp.status() == StatusCode::OK => {
-                trace!("heartbeat: OK");
-            }
-            Ok(resp) => {
-                let status = resp.status();
-                let body = resp.text().await?;
-                warn!("heartbeat: {}", status);
-                debug!("heartbeat: {}", body);
-            }
-            Err(err) => {
-                warn!("failed to send heartbeat to the server: {}", err);
-            }
-        };
+        trace!("sending heartbeat");
+        post_heartbeat(&client).await?;
 
         tokio::time::sleep(std::time::Duration::from_secs(5)).await;
     }
@@ -55,25 +65,14 @@ pub async fn heartbeat() -> Result<!> {
 pub async fn wait_for_server() {
     // before accepting tasks perform a synchronous heartbeat to ensure
     // the server has our worker ID recorded
+    let client = reqwest::Client::new();
+
     trace!("waiting for initial heartbeat");
     let mut retries = 5;
     loop {
         trace!("sending heartbeat");
-        match post_heartbeat(&reqwest::Client::new()).await {
-            Ok(resp) if resp.status() == reqwest::StatusCode::OK => {
-                trace!("heartbeat: OK");
-                break;
-            }
-            Ok(resp) => {
-                let status = resp.status();
-                let body = resp.text().await.expect("error getting response body");
-                warn!("heartbeat: {}", status);
-                debug!("heartbeat: {}", body);
-            }
-            Err(err) => {
-                info!("waiting for server...");
-                debug!("failed to send heartbeat to the server: {}", err);
-            }
+        if post_heartbeat(&client).await.expect("error posting heartbeat") {
+            break;
         }
 
         retries -= 1;
@@ -85,5 +84,5 @@ pub async fn wait_for_server() {
         tokio::time::sleep(std::time::Duration::from_secs(5)).await;
     }
 
-    info!("server received initial heartbeat, starting work");
+    trace!("server received initial heartbeat, starting work");
 }
