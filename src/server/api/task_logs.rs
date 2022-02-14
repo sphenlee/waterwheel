e886b67c201021d9be52d4b838cb5f4ec9cb5bbf
Steve Lee <slee@atlassian.com>
2022-02-14 20:06:53 +1100

    Revert "upgrade to highnoon 0.0.6"
    This reverts commit 14c7e4234175f9b061e0ca4ef9287e3d65715592.

--- src/server/api/task_logs.rs
+++ src/server/api/task_logs.rs
@@ -1,13 +1,13 @@
-use highnoon::ws::{WebSocketReceiver, WebSocketSender};
+use highnoon::ws::WebSocket;
 use highnoon::Message;
 use std::time::Duration;
 use tracing::info;
 
-pub async fn logs(mut tx: WebSocketSender, _rx: WebSocketReceiver) -> highnoon::Result<()> {
+pub async fn logs(mut ws: WebSocket) -> highnoon::Result<()> {
     loop {
         let msg = Message::text("Hello World!");
         info!("sending log line");
-        match tx.send(msg).await {
+        match ws.send(msg).await {
             Ok(_) => info!("send OK"),
             Err(e) => {
                 info!("send error: {}", e);
